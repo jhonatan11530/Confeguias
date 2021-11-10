cd0477385fc921f24a92b370b1287f230e4916b4
jhonatan fernandez <jhonatan1153@hotmail.com>
2021-11-10 12:00:15 -0500

    ok

--- routes/web.php
+++ routes/web.php
@@ -13,33 +13,32 @@ use Illuminate\Support\Facades\App;
 |
 */
 
-Route::get('/','PaginaWebController@PaginaWebIndex');
-Route::get('sobre-nosotros','PaginaWebController@PaginaWebNosotros');
-Route::get('directorio','PaginaWebController@PaginaWebDirectorio');
-Route::get('guias-nacionales','PaginaWebController@PaginaWebGuias');
-Route::get('contacto','PaginaWebController@PaginaWebContacto');
-Route::get('terminos','PaginaWebController@PaginaWebTerminos');
-Route::get('politicas','PaginaWebController@PaginaWebPoliticas');
-Route::get('beneficios','PaginaWebController@PaginaWebBeneficios');
-Route::get('denuncia','PaginaWebController@PaginaWebDenuncia');
+Route::get('/', 'PaginaWebController@PaginaWebIndex');
+Route::get('sobre-nosotros', 'PaginaWebController@PaginaWebNosotros');
+Route::get('directorio', 'PaginaWebController@PaginaWebDirectorio');
+Route::get('guias-nacionales', 'PaginaWebController@PaginaWebGuias');
+Route::get('contacto', 'PaginaWebController@PaginaWebContacto');
+Route::get('terminos', 'PaginaWebController@PaginaWebTerminos');
+Route::get('politicas', 'PaginaWebController@PaginaWebPoliticas');
+Route::get('beneficios', 'PaginaWebController@PaginaWebBeneficios');
+Route::get('denuncia', 'PaginaWebController@PaginaWebDenuncia');
 Route::post('Enviar/Contacto', 'MailController@MailContacto');
-Route::post('Enviar/Denuncia','MailController@MailDenuncia');
+Route::post('Enviar/Denuncia', 'MailController@MailDenuncia');
 
-Route::get('Registro/Guia','RegistroController@create');
-Route::post('Registro','RegistroController@store');
+Route::get('Registro/Guia', 'RegistroController@create');
+Route::post('Registro', 'RegistroController@store');
 Route::post('Registro/filtrar', 'RegistroController@filtrar');
 
 
 session_start();
 /*----- CAMBIAR IDIOMA LARAVEL -----*/
 if (empty($_SESSION['language'])) {
-    $_SESSION['language']="es";
+    $_SESSION['language'] = "es";
     App::setLocale('es');
-}else{
-    App::setLocale( $_SESSION['language']);
-    
+} else {
+    App::setLocale($_SESSION['language']);
 }
-Route::get('/change-lang/{lang}',function($lang){
-    $_SESSION['language']=$lang;
+Route::get('/change-lang/{lang}', function ($lang) {
+    $_SESSION['language'] = $lang;
     App::setLocale($lang);
 });
