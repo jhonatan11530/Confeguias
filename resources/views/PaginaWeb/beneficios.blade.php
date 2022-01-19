2618643c64feade973a0115373d6e5a77a78f949
jhonatan fernandez <jhonatan1153@hotmail.com>
2022-01-19 16:43:30 -0500

    mejora aplicada

--- resources/views/PaginaWeb/beneficios.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('Layout.plantilla')
-@section('contenido')
-<div class="directorio mb-5">
-    <div class="text-center center-block" style="color: white;">
-        <h1>BENEFICIOS</h1>
-    </div>
-</div>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,22 +12,7 @@ use Illuminate\Support\Facades\App;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', 'PaginaWebController@PaginaWebIndex');
-Route::get('sobre-nosotros', 'PaginaWebController@PaginaWebNosotros');
-Route::get('directorio', 'PaginaWebController@PaginaWebDirectorio');
-Route::get('guias-nacionales', 'PaginaWebController@PaginaWebGuias');
-Route::get('contacto', 'PaginaWebController@PaginaWebContacto');
-Route::get('terminos', 'PaginaWebController@PaginaWebTerminos');
-Route::get('politicas', 'PaginaWebController@PaginaWebPoliticas');
-Route::get('beneficios', 'PaginaWebController@PaginaWebBeneficios');
-Route::get('denuncia', 'PaginaWebController@PaginaWebDenuncia');
-Route::post('Enviar/Contacto', 'MailController@MailContacto');
-Route::post('Enviar/Denuncia', 'MailController@MailDenuncia');
-
-Route::post('guias-nacionales/Filtro', 'PaginaWebController@FiltroGuias');
-
-Route::get('Registro/Guia', 'RegistroController@create');
+Route::get('/', 'RegistroController@create');
 Route::post('Registro', 'RegistroController@store');
 Route::post('Registro/filtrar', 'RegistroController@filtrar');
 
